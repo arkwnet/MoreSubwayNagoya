058edb1e3d9accca5ef7a28099c45f36ca577d71
Sora Arakawa <mail@arkw.net>
2024-06-18 23:29:42 +0900

    Create Arakawa.png

--- Main.cpp
+++ Main.cpp
@@ -9,6 +9,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	int screenHeight = 768;
 	VECTOR camera;
 	float cameraZLength, cameraZDistance, cameraAngle;
+	int backgroundHandle[2];
+	int spriteHandle[16];
+	int soundHandle[16];
 
 	ChangeWindowMode(TRUE);
 	SetUseCharCodeFormat(DX_CHARCODEFORMAT_UTF8);
@@ -35,7 +38,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	Fps fps;
 	Game game = {
-		0,		// mode
+		-1,		// mode
 		0,		// status
 		0,		// count
 		0		// clock
@@ -44,7 +47,36 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0) {
 		fps.Update();
 		ClearDrawScreen();
-		// TODO: ここにメインプログラムを書く
+		switch (game.mode) {
+			case -1:
+				game.count = -10;
+				game.mode = 0;
+				break;
+			case 0:
+				SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+				if (game.count < 0) {
+					backgroundHandle[0] = LoadGraph(L"Assets\\Image\\Arakawa.png");
+					// soundHandle[0] = LoadSoundMem(L"Assets\\Sound\\Decide.wav");
+					game.count = 0;
+				}
+				if (game.count >= 30 && game.count < 45) {
+					SetDrawBlendMode(DX_BLENDMODE_ALPHA, (game.count - 30) * (255 / 15));
+				}
+				if (game.count >= 105 && game.count < 120) {
+					SetDrawBlendMode(DX_BLENDMODE_ALPHA, 255 - ((game.count - 105) * (255 / 15)));
+				}
+				if (game.count >= 30 && game.count < 120) {
+					DrawGraph(screenWidth / 2 - 301, screenHeight / 2 - 110, backgroundHandle[0], FALSE);
+				}
+				// if (game.count < 105 && (key[KEY_INPUT_SPACE] == 1 || joypad[PAD_3] == 1)) {
+				// 	game.count = 105;
+				// }
+				if (game.count == 180) {
+					game.count = -10;
+					game.mode = 1;
+				}
+				break;
+		}
 		game.count++;
 		game.clock++;
 		if (game.clock >= fps.Get()) {
