ff2e5149cc52264a2aec95568f536dab2c32817c
Sora Arakawa <mail@arkw.net>
2024-12-29 00:38:31 +0900

    Improve inverter frequency

--- Sound.cpp
+++ Sound.cpp
@@ -4,25 +4,17 @@
 void ControlVVVFSound(Navi navi, int soundHandle) {
 	if (CheckSoundMem(soundHandle) == 1) {
 		if (navi.p >= 1) {
-			if (navi.speed <= 3) {
-				SetFrequencySoundMem(37000, soundHandle);
-			} else if (navi.speed > 3 && navi.speed <= 8) {
-				SetFrequencySoundMem(37000 + (8000 * (navi.speed - 3)), soundHandle);
-			} else if (navi.speed > 8 && navi.speed <= 16) {
-				SetFrequencySoundMem(37000 + 40000, soundHandle);
-			} else if (navi.speed > 16 && navi.speed <= 25) {
-				SetFrequencySoundMem(37000 + 40000 + (2000 * (navi.speed - 16)), soundHandle);
+			if (navi.speed <= 20) {
+				SetFrequencySoundMem(22050, soundHandle);
 			} else {
-				SetFrequencySoundMem(16000 + (562 * (navi.speed - 25)), soundHandle);
+				SetFrequencySoundMem(8000 + (400 * (navi.speed - 20)), soundHandle);
 			}
 		}
 		if (navi.b >= 1) {
-			if (navi.speed <= 8) {
-				SetFrequencySoundMem(37000, soundHandle);
-			} else if (navi.speed > 8 && navi.speed <= 25) {
-				SetFrequencySoundMem(37000 + (2000 * (navi.speed - 8)), soundHandle);
+			if (navi.speed <= 20) {
+				SetFrequencySoundMem(22050, soundHandle);
 			} else {
-				SetFrequencySoundMem(16000 + (562 * (navi.speed - 25)), soundHandle);
+				SetFrequencySoundMem(8000 + (400 * (navi.speed - 20)), soundHandle);
 			}
 		}
 	}
