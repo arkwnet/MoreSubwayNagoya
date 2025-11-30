f4c46209ab8887dc48846c3c55b52825c2e76d17
Sora Arakawa <mail@arkw.net>
2025-11-30 15:03:13 +0900

    Replace some text with image for fast loading

--- Graphic.h
+++ Graphic.h
@@ -2,6 +2,7 @@
 #define _graphic_h
 
 void SetFont(const wchar_t* name, int size, int thick);
+void DrawNaviInteger(int y, int d, int spriteHandle);
 void DrawCab(int bufferHandle, int backgroundHandle, int spriteHandle, int tabletHandle, int stationHandle, Navi navi, Train train, int pressure, int ampere);
 
 #endif
